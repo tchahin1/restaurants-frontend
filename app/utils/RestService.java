496a98e07a765e99ecbf79a1406f765a5694f5b2
Tarek Shahin <tchahin1@etf.unsa.ba>
2019-05-10 07:50:10 +0200

    no message

--- app/utils/RestService.java
+++ app/utils/RestService.java
@@ -20,7 +20,7 @@ public class RestService {
     @Inject WSClient ws;
 
     protected WSRequest createRequest(String serviceUrl) {  //"http://localhost:8080" "https://abh-restaurants-backend.herokuapp.com"
-        return ws.url( "https://localhost:8080" + serviceUrl);
+        return ws.url( "http://localhost:8080" + serviceUrl);
     }
 
     protected WSRequest createFullPathRequest(String serviceUrl) {
